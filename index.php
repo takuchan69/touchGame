c263071a5247715972f5146b4653da3ee68e168c
takuchan69 <57449179+takuchan69@users.noreply.github.com>
2020-08-21 18:26:58 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+<!DOCTYPE html>
+
+<head>
+       <meta chraset='UTF-8'>
+       <meta name='description' content='make number touch game for practice of JavaScript'>
+       <meta name='author' content='takuchan69'>
+       <link rel='stylesheet' href='styles.css'>
+       <title>TouchNumber</title>
+</head>
+<body>
+       <header>
+       <h1>Try Touch Number Game</h1>
+       </header>
+       <section id='container'>
+       </section>
+       <footer>
+       <p>Refered from Dotinstall</p>
+       </footer>
+</body>
+</html>
